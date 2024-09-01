e38bd717edfb0efcb4c9c1fd0bebc4efd81aaa66
Yuli-5 <moraleslisbeth2005@gmail.com>
2024-09-01 14:23:20 -0600

    Jugador

--- src/exposicion_ahorcado/Jugador.java
+++ src/exposicion_ahorcado/Jugador.java
@@ -10,4 +10,53 @@ package exposicion_ahorcado;
  */
 public class Jugador {
     
+   // Atributos para el jugador
+    private String nombre;
+    private int puntuacion;
+    private char[] letrasIntentadas;
+    private int vidasRestantes;
+    private int juegosGanados;
+    
+    //Setter y getter
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPuntuacion() {
+        return puntuacion;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+        this.puntuacion = puntuacion;
+    }
+
+    public char[] getLetrasIntentadas() {
+        return letrasIntentadas;
+    }
+
+    public void setLetrasIntentadas(char[] letrasIntentadas) {
+        this.letrasIntentadas = letrasIntentadas;
+    }
+
+    public int getVidasRestantes() {
+        return vidasRestantes;
+    }
+
+    public void setVidasRestantes(int vidasRestantes) {
+        this.vidasRestantes = vidasRestantes;
+    }
+
+    public int getJuegosGanados() {
+        return juegosGanados;
+    }
+
+    public void setJuegosGanados(int juegosGanados) {
+        this.juegosGanados = juegosGanados;
+    }
+    
 }
